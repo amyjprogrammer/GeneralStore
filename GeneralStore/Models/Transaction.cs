ecd5b2bd82e42113ae1ef959a67662f91e586a99
amyjprogrammer <amyjprogrammer@gmail.com>
2021-10-14 21:51:41 -0400

    adding transaction

---
+++ GeneralStore/Models/Transaction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Customer))]
+        public int CustomerId { get; set; }
+
+        //Navigation Property
+        public virtual Customer Customer { get; set; }
+
+        [ForeignKey(nameof(Product))]
+        public string ProductSKU { get; set; }
+        public virtual Product Product { get; set; }
+
+        public int ItemCount
+        {
+            get 
+            {
+                int count = 0;
+                foreach (var item in Product.SKU)
+                {
+                    count = count + item;
+                }
+                return count;
+            }
+        }
+
+        public DateTime DateOfTransaction { get; set; }
+    }
+}
